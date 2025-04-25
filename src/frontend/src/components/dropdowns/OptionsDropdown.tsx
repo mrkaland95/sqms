e07c3cc8d84ad16e68d0f392820c8b173b6eeeea
Flaxelaxen <mrkaland95@gmail.com>
2025-04-25 18:51:19 +0200

    Dropdown menu for further options nearly complete.

---
+++ src/frontend/src/components/dropdowns/OptionsDropdown.tsx
@@ -0,0 +1,56 @@
+import React, {useEffect, useRef, useState} from "react";
+import './options.css'
+
+function OptionsDropdown(props: {buttonText?: string, children?: React.ReactNode, size?: number}) {
+    const [menuOpen, setMenuOpen] = useState(false)
+    const dropdownRef = useRef<HTMLDivElement | null>(null);
+    // const optionsIcon = '⋮'
+    const optionsIcon = '…'
+
+
+
+    // Adds functionality that closes the menu if the user clicks outside of it.
+    useEffect(() => {
+        const handleClickOutside = (event: any) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setMenuOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+    }, []);
+
+    function toggleMenu() {
+        setMenuOpen(!menuOpen)
+    }
+
+    return (
+        <div className={`options-menu-wrapper ${menuOpen ? 'open' : ''}`} ref={dropdownRef}>
+            <button
+                className={`options-menu-button ${menuOpen ? 'open' : ''}`}
+                type={"button"} onClick={toggleMenu}
+                style={{fontSize: props.size}}>
+                {optionsIcon}
+            </button>
+            <ul className={`options-menu-content ${menuOpen ? 'open' : ''}`}>
+                {props.children}
+            </ul>
+        </div>)
+}
+
+
+export function OptionsItem(props: {title?: string, onClick?: () => void, leftIcon?: any, rightIcon?: any, children?: React.ReactNode}) {
+    return (
+    <li className={"options-menu-item"} title={props.title} onClick={() => props.onClick && props.onClick()}>
+        <span>{props.leftIcon}</span>
+        {props.children}
+        <span>{props.rightIcon}</span>
+    </li>)
+}
+
+export function OptionsDivider(props: {}) {
+    return (<div className={"options-menu-divider"}></div>)
+}
+
+
+
+export default OptionsDropdown;
